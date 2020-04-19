fdc916e62acab42eff8d510d0a1edac745cd093d
Daniel Jones <messyjones@gmail.com>
2020-04-18 21:16:55 -0500

    added important info for the server configuration

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_swmb05cm:5rbvq77vsuvgn3v49ljtobhl8@ds115738.mlab.com:15738/heroku_swmb05cm',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'triviaSite19350504956904',
+  masterKey: process.env.MASTER_KEY || 'fsdkldfgjldfkgGFJJF495849405', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    classNames: [] // List of classes to support for query subscriptions
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
